0b61f38444eb1c1d2eb9f2bed6f5b6e6133cd733
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-09-07 21:13:33 +0200

    Lesson03 add

---
+++ src/homework_02/HomeWork_02.java
@@ -0,0 +1,23 @@
+package homework_02;
+
+public class HomeWork_02 {
+    public static void main(String[] args) {
+        // Ctrl + Alt + L - выравнивание кода
+
+        // "" <> # ''}{ [] <> + - = ; : ,  /
+
+        System.out.println("Hello, World!");
+        System.out.println("Second line");
+
+        int myAge = 18;
+        double priceLiterMilk = 1.10;
+        float floatVar = 1.10f;
+
+        String myName = "Vasya";
+
+        System.out.println("My name is: " + myName);
+        System.out.println("My age is: " + myAge);
+        System.out.println("Price of 1l milk: " + priceLiterMilk);
+
+    }
+}
