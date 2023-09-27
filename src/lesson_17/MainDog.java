5801d7abcc66251c6348eac64c0a0dcdfdfa3ebd
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-09-27 21:02:31 +0200

    lesson_17 code add

---
+++ src/lesson_17/MainDog.java
@@ -0,0 +1,22 @@
+package lesson_17;
+/*
+@date 27.09.2023
+@author Sergey Bugaienko
+*/
+
+public class MainDog {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Bim", 5);
+        System.out.println(dog.whoAmI());
+
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+
+        System.out.println(dog.whoAmI());
+    }
+}
