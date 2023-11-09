f826be155dd4df5f259798cde221462c6037dcfc
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-11-09 12:06:16 +0100

    lesson_46 add

---
+++ src/lesson_46/sets/Car.java
@@ -0,0 +1,59 @@
+package lesson_46.sets;
+/*
+@date 08.11.2023
+@author Sergey Bugaienko
+*/
+
+import java.util.Objects;
+
+public class Car {
+    private String model;
+    private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (year != car.year) return false;
+        return Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = model != null ? model.hashCode() : 0;
+        result = 31 * result + year;
+        return result;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
